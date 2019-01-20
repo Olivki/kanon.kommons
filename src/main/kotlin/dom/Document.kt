f84681e506bc947e59b0b0b998b2763b401736a1
Olivki <oliver@berg.moe>
2019-01-20 22:34:16 +0100

    Added all "append..." functions to the Document.
    Signed-off-by: Olivki <oliver@berg.moe>

--- src/main/kotlin/dom/Document.kt
+++ src/main/kotlin/dom/Document.kt
@@ -16,3 +16,60 @@
 
 package moe.kanon.kextensions.dom
 
+import org.w3c.dom.*
+
+/**
+ * Creates an [Element] from the specified [tagName] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendElement(tagName: String): Element =
+    this.appendChild(this.createElement(tagName))!! as Element
+
+/**
+ * Creates an [Element] from the given [namespaceURI] and [qualifiedName] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendElementNS(namespaceURI: String?, qualifiedName: String): Element =
+    this.appendChild(this.createElementNS(namespaceURI, qualifiedName))!! as Element
+
+/**
+ * Creates a [Comment] of the specified [data] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendComment(data: String): Comment =
+    this.appendChild(this.createComment(data))!! as Comment
+
+/**
+ * Creates a [TextNode][Text] of the specified [data] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendTextNode(data: String): Text =
+    this.appendChild(this.createTextNode(data))!! as Text
+
+/**
+ * Creates a [CDATASection] of the specified [data] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendCDataSection(data: String): CDATASection =
+    this.appendChild(this.createCDATASection(data))!! as CDATASection
+
+/**
+ * Creates a [ProcessingInstruction] from the specified [target] and [data] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendProcessingInstruction(target: String, data: String): ProcessingInstruction =
+    this.appendChild(this.createProcessingInstruction(target, data))!! as ProcessingInstruction
+
+/**
+ * Creates a [EntityReference] from the specified [name] and then appends it to this document.
+ *
+ * @since 0.4.0
+ */
+public fun Document.appendEntityReference(name: String): EntityReference =
+    this.appendChild(this.createEntityReference(name))!! as EntityReference
