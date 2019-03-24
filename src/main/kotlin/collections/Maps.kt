15034dd68ebd2f20735e1790b9f2bbdfb5b07511
Oliver Berg <oliver@berg.moe>
2019-03-24 21:35:11 +0100

    Add "enumMapOf" top-level function
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- src/main/kotlin/collections/Maps.kt
+++ src/main/kotlin/collections/Maps.kt
@@ -66,6 +66,18 @@ public fun <K, V> MutableMap<K, V>.inherit(from: Map<out K, V>) {
 public fun <K, V> treeMapOf(vararg pairs: Pair<K, V>): TreeMap<K, V> = TreeMap(pairs.toMap())
 
 /**
+ * Creates and returns a [EnumMap] containing the specified [entries].
+ *
+ * @since 0.6.0
+ */
+// we're using the enum class constructor rather than the one that accepts a map so that the function won't fail if
+// the supplied 'entries' is empty.
+public inline fun <reified EK : Enum<EK>, V> enumMapOf(vararg entries: Pair<EK, V>): EnumMap<EK, V> =
+    EnumMap<EK, V>(EK::class.java).apply {
+        for ((key, value) in entries) this[key] = value
+    }
+
+/**
  * Stores the specified [value] under the specified [key] in `this` map, and then returns [value].
  *
  * @return [value]
