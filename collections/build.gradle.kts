8c236834baa168dab9581ce38160b8047abe6967
Oliver Berg <oliver@berg.moe>
2019-08-21 17:29:18 +0200

    Add top-level functions for creating 'EnumSet' instances
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- collections/build.gradle.kts
+++ collections/build.gradle.kts
@@ -1,3 +1,3 @@
 description = "Provides utilities/extensions and data structures for working with collections in Kotlin"
-version = "0.2.0"
+version = "0.3.0"
 extra["packageName"] = "collections"
--- collections/src/main/kotlin/moe/kanon/kommons/collections/Sets.kt
+++ collections/src/main/kotlin/moe/kanon/kommons/collections/Sets.kt
@@ -18,7 +18,9 @@
 
 package moe.kanon.kommons.collections
 
+import java.nio.file.StandardOpenOption
 import java.util.*
+import kotlin.reflect.KClass
 
 // -- CLASSES -- \\
 /**
@@ -46,7 +48,70 @@ fun <T> Set<T>.asUnmodifiableSet(): Set<T> = Collections.unmodifiableSet(this)
  * Returns a new [singleton set][SingletonSet] wrapped around the specified [item].
  */
 @JvmName("singletonOf")
-fun <E> singletonSet(item: E): Set<E> = SingletonSet(item)
+fun <T> singletonSet(item: T): Set<T> = SingletonSet(item)
+
+/**
+ * Converts `this` set to a [EnumSet].
+ */
+inline fun <reified E : Enum<E>> Set<E>.toEnumSet(): EnumSet<E> = convertToEnumSet(this, E::class.java)
+
+/**
+ * Returns a new [EnumSet] that initially contains the all of the elements in `this` range.
+ */
+fun <E : Enum<E>> ClosedRange<E>.toEnumSet(): EnumSet<E> = EnumSet.range(this.start, this.endInclusive)
+
+@PublishedApi
+internal fun <E : Enum<E>> convertToEnumSet(origin: Set<E>, clz: Class<E>): EnumSet<E> = when (origin) {
+    is EnumSet<E> -> origin
+    else -> EnumSet.noneOf(clz).apply { addAll(origin) }
+}
+
+/**
+ * Returns a new and empty [EnumSet] for the given [enum][E].
+ */
+inline fun <reified E : Enum<E>> emptyEnumSet(): EnumSet<E> = EnumSet.noneOf(E::class.java)
+
+/**
+ * Returns a new [EnumSet] containing all the enum constants of the given [enum][E].
+ */
+inline fun <reified E : Enum<E>> enumSetOf(): EnumSet<E> = emptyEnumSet<E>().apply { addAll(enumValues<E>()) }
+
+/**
+ * Returns a new [EnumSet] for the given [enum][E] that initially contains the given element.
+ */
+fun <E : Enum<E>> enumSetOf(e: E): EnumSet<E> = EnumSet.of(e)
+
+/**
+ * Returns a new [EnumSet] for the given [enum][E] that initially contains the given elements.
+ */
+fun <E : Enum<E>> enumSetOf(e1: E, e2: E): EnumSet<E> = EnumSet.of(e1, e2)
+
+/**
+ * Returns a new [EnumSet] for the given [enum][E] that initially contains the given elements.
+ */
+fun <E : Enum<E>> enumSetOf(e1: E, e2: E, e3: E): EnumSet<E> = EnumSet.of(e1, e2, e3)
+
+/**
+ * Returns a new [EnumSet] for the given [enum][E] that initially contains the given elements.
+ */
+fun <E : Enum<E>> enumSetOf(e1: E, e2: E, e3: E, e4: E): EnumSet<E> = EnumSet.of(e1, e2, e3, e4)
+
+/**
+ * Returns a new [EnumSet] for the given [enum][E] that initially contains the given elements.
+ */
+fun <E : Enum<E>> enumSetOf(e1: E, e2: E, e3: E, e4: E, e5: E): EnumSet<E> = EnumSet.of(e1, e2, e3, e4, e5)
+
+/**
+ * Returns a new [EnumSet] for the given [enum][E] that initially contains the [initial] element, and the [rest].
+ */
+fun <E : Enum<E>> enumSetOf(initial: E, vararg rest: E): EnumSet<E> = when (rest.size) {
+    0 -> enumSetOf(initial)
+    1 -> enumSetOf(initial, rest[0])
+    2 -> enumSetOf(initial, rest[0], rest[1])
+    3 -> enumSetOf(initial, rest[0], rest[1], rest[2])
+    4 -> enumSetOf(initial, rest[0], rest[1], rest[2], rest[3])
+    else -> EnumSet.of(initial, *rest)
+}
 
 // -- UTIL FUNCTIONS -- \\
 
