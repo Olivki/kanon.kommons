4e94196ae50e479fc6d1fd65588ad8a9c2472644
Oliver Berg <oliver@berg.moe>
2020-04-22 19:53:37 +0200

    [lang-0.6.0] Deprecated 'Any?.hash', 'Any?.identityHash' and 'Any?.string'
    These properties have been deprecated and replaced with the functions 'hashOf(value)', 'identityHashOf(value)' and 'stringOf(value)'. They were deprecated because as extension properties they were polluting the namespace of every single value for no real gain, the new functions also behave slightly differently.
    
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- lang/src/main/kotlin/moe/kanon/kommons/lang/Anys.kt
+++ lang/src/main/kotlin/moe/kanon/kommons/lang/Anys.kt
@@ -19,30 +19,73 @@
 package moe.kanon.kommons.lang
 
 /**
- * Returns the [hash][Any.hashCode] of `this` value, or `0` if `this` is `null`.
- */
-val Any?.hash: Int
-    @JvmName("hashOf")
-    get() = this?.hashCode() ?: 0
-
-/**
  * Returns the same hash code for the given object as would be returned by the default [Any.hashCode] function,
  * regardless of whether or not `this` value's class overrides `hashCode`, or `0` if `this` is `null`.
  *
  * @see System.identityHashCode
  */
+@Deprecated(
+    message = "Use 'identityHashOf(this)' instead.",
+    replaceWith = ReplaceWith("identityHashOf(this)", "moe.kanon.kommons.lang")
+)
 val Any?.identityHash: Int
-    @JvmName("identityHashOf")
     get() = System.identityHashCode(this)
+
+/**
+ * Returns the [hash][Any.hashCode] of `this` value, or `0` if `this` is `null`.
+ */
+@Deprecated(
+    message = "Use 'hashOf(this)' instead.",
+    replaceWith = ReplaceWith("hashOf(this)", "moe.kanon.kommons.lang")
+)
+val Any?.hash: Int
+    get() = this?.hashCode() ?: 0
 
 /**
  * Attempts to always return a human-readable `String` representation of `this` string.
  */
+@Deprecated(
+    message = "Use 'stringOf(this)' instead.",
+    replaceWith = ReplaceWith("stringOf(this)", "moe.kanon.kommons.lang")
+)
 val Any?.string: String
-    @JvmName("stringOf")
     get() = when (this) {
         null -> "null"
         is Array<*> -> this.contentDeepToString()
         is Iterable<*> -> "[${this.joinToString()}]"
         else -> this.toString()
     }
+
+/**
+ * Returns the same hash code for the given object as would be returned by the default [Any.hashCode] function,
+ * regardless of whether or not the class of [value] overrides `hashCode`, or `0` if `value` is `null`.
+ *
+ * @see System.identityHashCode
+ */
+fun identityHashOf(value: Any?): Int = value?.let(System::identityHashCode) ?: 0
+
+/**
+ * Returns the hash of [value], or `0` if `value` is `null`.
+ *
+ * If `value` is an `Array` then this returns [Array.contentDeepHashCode], if it's a `Iterable` or a `Sequence` then it
+ * will calculate the hashcode from all the hashcodes of the elements contained within it and return that.
+ */
+fun hashOf(value: Any?): Int = when (value) {
+    null -> 0
+    is Array<*> -> value.contentDeepHashCode()
+    is Iterable<*> -> value.fold(1) { hash, element -> 31 * hash + hashOf(element) }
+    is Sequence<*> -> value.fold(1) { hash, element -> 31 * hash + hashOf(element) }
+    else -> value.hashCode()
+}
+
+/**
+ * Attempts to always return a human-readable `String` representation of [value].
+ */
+fun stringOf(value: Any?): String = when (value) {
+    null -> "null"
+    is Array<*> -> value.contentDeepToString()
+    is Iterable<*> -> "[${value.joinToString(transform = ::stringOf)}]"
+    is Sequence<*> -> "[${value.joinToString(transform = ::stringOf)}]"
+    is Map<*, *> -> "{${value.entries.joinToString { (key, value) -> "${stringOf(key)}: ${stringOf(value)}" }}}"
+    else -> value.toString()
+}
