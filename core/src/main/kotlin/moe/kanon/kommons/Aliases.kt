800191c2a736377a4ee0eb8261095e8b99696be6
Oliver Berg <oliver@berg.moe>
2020-04-22 17:20:15 +0200

    [1.3.0-core] Deprecate faulty type-aliases
    The Kotlin compiler does not properly handle the usage of type-aliased annotations, meaning that if a function was annotated with '@static' rather than '@JvmStatic' the function would not actually be compiled to a static function in the java bytecode, the same applies to the rest of the deprecated type-aliases.
    
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- core/src/main/kotlin/moe/kanon/kommons/Aliases.kt
+++ core/src/main/kotlin/moe/kanon/kommons/Aliases.kt
@@ -5,7 +5,30 @@ package moe.kanon.kommons
  */
 typealias StringMap<T> = Map<String, T>
 
+@Deprecated(
+    message = "Does not work, use 'JvmStatic' instead.",
+    replaceWith = ReplaceWith("JvmStatic", "kotlin.jvm"),
+    level = DeprecationLevel.ERROR
+)
 typealias static = JvmStatic
+
+@Deprecated(
+    message = "Does not work, use 'JvmSynthetic' instead.",
+    replaceWith = ReplaceWith("JvmSynthetic", "kotlin.jvm"),
+    level = DeprecationLevel.ERROR
+)
 typealias synthetic = JvmSynthetic
+
+@Deprecated(
+    message = "Does not work, use 'JvmName' instead.",
+    replaceWith = ReplaceWith("JvmName", "kotlin.jvm"),
+    level = DeprecationLevel.ERROR
+)
 typealias name = JvmName
+
+@Deprecated(
+    message = "Does not work, use 'JvmOverloads' instead.",
+    replaceWith = ReplaceWith("JvmOverloads", "kotlin.jvm"),
+    level = DeprecationLevel.ERROR
+)
 typealias overloads = JvmOverloads
