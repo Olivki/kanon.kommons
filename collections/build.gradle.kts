cdeb197f349d10ed2874e9ae4a45362f7310e03e
Oliver Berg <oliver@berg.moe>
2020-05-20 18:31:03 +0200

    [collections] Change version to '0.13.0'
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- collections/build.gradle.kts
+++ collections/build.gradle.kts
@@ -1,3 +1,3 @@
 description = "Provides utilities/extensions and data structures for working with collections in Kotlin"
-version = "0.12.0"
+version = "0.13.0"
 extra["packageName"] = "collections"
