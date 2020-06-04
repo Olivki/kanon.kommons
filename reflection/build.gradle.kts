4c55c24e8a2966ce3c8019d9a3f64524ce49d122
Oliver Berg <oliver@berg.moe>
2020-06-04 14:13:40 +0200

    [reflection] Remove unneeded code from build file
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- reflection/build.gradle.kts
+++ reflection/build.gradle.kts
@@ -1,18 +1,7 @@
-plugins {
-    kotlin("kapt")
-}
-
 description = "Provides some basic utilities for working with reflection in Kotlin"
 version = "0.6.0"
 extra["packageName"] = "reflection"
 
 dependencies {
     api(kotlin("reflect"))
-
-    // tests
-    testImplementation(group = "io.kotlintest", name = "kotlintest-runner-junit5", version = "3.1.11")
-    testImplementation(group = "org.slf4j", name = "slf4j-simple", version = "1.8.0-beta2")
-
-    testCompileOnly(group = "com.google.auto.service", name = "auto-service", version = "1.0-rc4")
-    kaptTest(group = "com.google.auto.service", name = "auto-service", version = "1.0-rc4")
 }
